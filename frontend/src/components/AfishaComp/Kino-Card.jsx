85e32a164d1e25ff82c4c2ac42b5131da61fbb71
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2024-08-30 15:13:05 +0900

    update

--- frontend/src/components/AfishaComp/Kino-Card.jsx
+++ frontend/src/components/AfishaComp/Kino-Card.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 import '../../style/Kino_Card.css'
 
 export default function Kino_Card({title, tags, chrono, urlKino, urlBuyTicket, poster, inTime}) {
   return (
     <div className="kino__card">
-        <img src={poster}/>
-        <div className="title">{title}</div>
-        <div className="tags_time">{tags}, {chrono}</div>
+        <Link to={urlKino}><img src={poster}/></Link>
+        <div className="content">
+          <div className="title">{title}</div>
+          <div className="tags_time">{tags}, {chrono}</div>
+          <div className="urlBuyTicketsDiv"><Link className='urlBuyTicket' to={urlBuyTicket}>Купить билет</Link></div>
+        </div>
     </div>
   )
 }
